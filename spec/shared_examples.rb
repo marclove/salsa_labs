64b30aea3e3a094d1e2a98117be48000fab51f9d
Marc Love <marcslove@gmail.com>
2013-08-07 10:56:11 -0700

    Fixtures

---
+++ spec/shared_examples.rb
@@ -0,0 +1,19 @@
+=begin
+shared_examples_for 'a SalsaObject' do
+  it 'knows its object name' do
+    expect(klass.object_name).to eq(object_name)
+  end
+  
+  it '#schema' do
+  end
+  
+  it '#get' do
+  end
+  
+  it '#count' do
+  end
+  
+  it '#all' do
+  end
+end
+=end
